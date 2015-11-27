d5ab9ff3a35a1823c45f01939d35f1c764c0a3be
Benjapol Worakan <benwrk@live.com>
2015-11-28 04:48:13 +0700

    Uncomment sources

--- src/PrimeFactors.cpp
+++ src/PrimeFactors.cpp
@@ -1,47 +1,47 @@
-//#include <iostream>
-//#include <sstream>
-//#include <cmath>
-//#include <vector>
-//
-//using namespace std;
-//
-//int factors(int n)
-//{
-//	int z = 2;
-//	int last = 0;
-//	int factors = 0;
-//	
-//	while (z * z <= n)
-//	{
-//		if (n % z == 0)
-//		{
-//			if (last != z)
-//			{
-//				last = z;
-//				factors++;
-//			}
-//			n /= z;
-//		}
-//		else
-//		{
-//			z++;
-//		}
-//	}
-//	if (n > 1)
-//	{
-//		if (last != n)
-//		{
-//			last = n;
-//			factors++;
-//		}
-//	}
-//	return factors;
-//}
-//
-//int main()
-//{
-//	int n;
-//	cin >> n;
-//	cout << factors(n) << endl;
-//	return 0;
-//}
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int factors(int n)
+{
+	int z = 2;
+	int last = 0;
+	int factors = 0;
+	
+	while (z * z <= n)
+	{
+		if (n % z == 0)
+		{
+			if (last != z)
+			{
+				last = z;
+				factors++;
+			}
+			n /= z;
+		}
+		else
+		{
+			z++;
+		}
+	}
+	if (n > 1)
+	{
+		if (last != n)
+		{
+			last = n;
+			factors++;
+		}
+	}
+	return factors;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << factors(n) << endl;
+	return 0;
+}
--- src/WebCache.cpp
+++ src/WebCache.cpp
@@ -1,47 +1,34 @@
-//#include <iostream>
-//#include <set>
-//#include <algorithm>
-//
-//using namespace std;
-//
-////bool contains(int x, set<int> pages)
-////{
-////	set<int>::iterator pagesItr;
-////	for (pagesItr = pages.begin(); pagesItr != pages.end(); pagesItr++)
-////	{
-////		if (*pagesItr == x)
-////		{
-////			return 1;
-////		}
-////	}
-////	return 0;
-////}
-//
-//int main()
-//{
-//	int misses = 0;
-//	int n;
-//	cin >> n;
-//	int m;
-//	cin >> m;
-//	set<int> pages;
-//	set<int>::iterator pagesItr;
-//	for (int i = 0; i < m; i++)
-//	{
-//		int x;
-//		cin >> x;
-//		if (!(find(pages.begin(), pages.end(), x) != pages.end()))
-//		{
-//			misses++;
-//			/*cout << "MISS" << endl;*/
-//			pages.insert(x);
-//			if (pages.size() > n)
-//			{
-//				/*cout << "THROW" << pages.front() << endl;*/
-//				pages.erase(*pages.begin());
-//			}
-//		}
-//	}
-//	cout << misses << endl;
-//	return 0;
-//}
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int misses = 0;
+	int n;
+	cin >> n;
+	int m;
+	cin >> m;
+	set<int> pages;
+	set<int>::iterator pagesItr;
+	for (int i = 0; i < m; i++)
+	{
+		int x;
+		cin >> x;
+		if (!(find(pages.begin(), pages.end(), x) != pages.end()))
+		{
+			misses++;
+			/*cout << "MISS" << endl;*/
+			pages.insert(x);
+			if (pages.size() > n)
+			{
+				/*cout << "THROW" << pages.front() << endl;*/
+				pages.erase(*pages.begin());
+			}
+		}
+	}
+	cout << misses << endl;
+	return 0;
+}
